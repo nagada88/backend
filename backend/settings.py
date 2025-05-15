e0c66077d12b8cc49d02d4356caa7bedd2037e05
Nagy <nagada88@gmail.com>
2025-05-15 10:21:29 +0200

    Creating REST api, and interface for file upload

--- backend/settings.py
+++ backend/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'analyzer',
 ]
 
 MIDDLEWARE = [
@@ -48,7 +50,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True  # Fejlesztéshez elég
 
 ROOT_URLCONF = 'backend.urls'
 
